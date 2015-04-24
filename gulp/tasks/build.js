9a028874864fd86f82c5f1d63772acce17a21fc7
shimizu_yasuhiro <shimizu_yasuhiro@cyberagent.co.jp>
2015-04-24 11:58:09 +0900

    fix gulp

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,6 +3,6 @@
 var gulp = require('gulp');
 var config = require('../config').watch;
 
-gulp.task('build', ['webpack', 'styles', 'html'], function() {
+gulp.task('build', ['babel', 'styles', 'html', 'electron'], function() {
     gulp.src(config.src);
 });
